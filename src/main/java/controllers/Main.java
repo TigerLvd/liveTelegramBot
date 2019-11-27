a59bf6fee393122482d5afbc9195eb024da481f6
lvd <DABorisenko@lanit.ru>
2019-11-27 22:14:39 +0500

    Рефакторинг + убрал лишнее.

--- src/main/java/controllers/Main.java
+++ src/main/java/controllers/Main.java
@@ -11,13 +11,10 @@ import java.io.IOException;
 import java.util.Properties;
 
 import model.homeGroups.HomeGroupBot;
-import model.liveInfo.LiveInfoBot;
 
 public class Main {
     private static String BOT_NAME;
     private static String BOT_TOKEN;
-    private static String BOT_NAME2;
-    private static String BOT_TOKEN2;
 
     private static String PROXY_HOST;
     private static Integer PROXY_PORT;
@@ -35,7 +32,6 @@ public class Main {
 
             DefaultBotOptions botOptions = ApiContext.getInstance(DefaultBotOptions.class);
             HomeGroupBot bot;
-            LiveInfoBot bot2;
             if (Boolean.TRUE.equals(USE_PROXY)) {
                 // use PROXY
                 botOptions.setProxyHost(PROXY_HOST);
@@ -43,15 +39,12 @@ public class Main {
                 botOptions.setProxyType(DefaultBotOptions.ProxyType.SOCKS5);
 
                 bot = new HomeGroupBot(BOT_TOKEN, BOT_NAME, ADMIN_ID, botOptions);
-                bot2 = new LiveInfoBot(BOT_TOKEN2, BOT_NAME2, botOptions);
             } else {
                 // use VPN
                 bot = new HomeGroupBot(BOT_TOKEN, BOT_NAME, ADMIN_ID);
-                bot2 = new LiveInfoBot(BOT_TOKEN2, BOT_NAME2);
             }
 
             botsApi.registerBot(bot);
-            botsApi.registerBot(bot2);
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
@@ -68,8 +61,6 @@ public class Main {
 
             BOT_NAME = property.getProperty("bot_name");
             BOT_TOKEN = property.getProperty("bot_token");
-            BOT_NAME2 = property.getProperty("bot_name2");
-            BOT_TOKEN2 = property.getProperty("bot_token2");
 
             PROXY_HOST = property.getProperty("proxy_host");
             PROXY_PORT = new Integer(property.getProperty("proxy_port"));
@@ -77,7 +68,6 @@ public class Main {
             ADMIN_ID = new Long(property.getProperty("admin_id"));
 
             System.out.println("BOT_NAME: " + BOT_NAME
-                    + ", BOT_NAME2: " + BOT_NAME2
                     + ", PROXY_HOST: " + PROXY_HOST
                     + ", PROXY_PORT: " + PROXY_PORT);
 
